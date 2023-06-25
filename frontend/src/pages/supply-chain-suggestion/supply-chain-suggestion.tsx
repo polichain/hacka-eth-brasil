05d6a74b8a3c27c5db42ef2315440d45476fc3f2
Tiago Marinho <tiago.limarinho@gmail.com>
2023-06-24 21:13:39 -0300

    supply chain suggestion page

--- frontend/src/pages/supply-chain-suggestion/supply-chain-suggestion.tsx
+++ frontend/src/pages/supply-chain-suggestion/supply-chain-suggestion.tsx
@@ -2,56 +2,51 @@ import { Button, TextField, Typography } from "@mui/material";
 import { FormProvider, useForm } from "react-hook-form";
 import contractConfig from "../../contracts/contract-config.json";
 import { Address, useContractWrite } from "wagmi";
-import { Pagination } from "../../types";
+import { Company, Pagination } from "../../types";
+import { LoginPage } from "../login";
 
-interface SupplyChainCreatePageProps {
+interface SupplyChainSuggestionPageProps {
   setCurrentPage: (page: Pagination) => void;
 }
 
-export const SupplyChainCreatePage: React.FC<SupplyChainCreatePageProps> = ({
+export const SupplyChainSuggestionPage: React.FC<SupplyChainSuggestionPageProps> = ({
   setCurrentPage,
-}: SupplyChainCreatePageProps) => {
+}: SupplyChainSuggestionPageProps) => {
   const form = useForm();
   const { handleSubmit, register } = form;
 
   const { write, data, error, isLoading, isError } = useContractWrite({
     address: contractConfig.address as Address,
     abi: contractConfig.abi,
-    functionName: "createSupplyChain",
+    functionName: "createSupplyChain", // mudar aq
   });
 
-  const handleSupplyChainSubmit = (data: any) => {
+  const handleSupplyChainSuggestionSubmit = (data: any) => {
     write?.({
       args: [data.name, data.description],
     });
-    setCurrentPage(Pagination.SupplyChainList);
+    setCurrentPage(Pagination.Login); // mudar
   };
 
   return (
     <FormProvider {...form}>
-      <form onSubmit={handleSubmit((data) => handleSupplyChainSubmit(data))}>
+      <form onSubmit={handleSubmit((data) => handleSupplyChainSuggestionSubmit(data))}>
         <div className="d-flex flex-column align-items-center px-3 pt-3 gap-3">
           <Typography variant="h6">
-            Complete o cadastro de sua cadeia de suprimentos:
+            Digite uma sugestão para a cadeia de suprimentos:
           </Typography>
 
         <TextField
-            id="name"
-            {...register("name", { required: true })}
-            label="Nome da Cadeia de Suprimentos"
-            sx={{ width: "50%" }}
-        />
-        <TextField
-            id="description"
-            {...register("description", { required: true })}
-            label="Descrição"
+            id="suggestion"
+            {...register("suggestion", { required: true })}
+            label="Sugestão"
             sx={{ width: "50%" }}
         />
 
         <div className="d-flex flex-column align-items-center w-30">
         <Button variant="outlined" type="submit">
             <Typography variant="subtitle2" fontWeight="600">
-            Criar cadeia
+            Publicar sugestão
             </Typography>
         </Button>
         </div>
